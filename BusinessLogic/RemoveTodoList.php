f831ee9da414b9817a7318ca5a418935e0e28727
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2024-08-10 23:31:52 +0700

    feat(logic): add code for function removeTodoList

--- BusinessLogic/RemoveTodoList.php
+++ BusinessLogic/RemoveTodoList.php
@@ -2,8 +2,26 @@
 
 /**
  * Menghapus todo list
+ * 
+ * @param int $number
+ * @return bool
  */
-function removeTodoList()
+function removeTodoList(int $number): bool
 {
-  //
+    // Mengambil data dari global scope
+    global $todoList;
+
+    if ($number > sizeof($todoList)) {
+        return false;
+    }
+
+    // Mengurutkan array dengan menggeser data ke index sebelumnya
+    for ($i = $number; $i < sizeof($todoList); $i++) {
+        $todoList[$i] = $todoList[$i + 1];
+    }
+
+    // Menghapus data terakhir
+    unset($todoList[sizeof($todoList)]);
+
+    return true;
 }
