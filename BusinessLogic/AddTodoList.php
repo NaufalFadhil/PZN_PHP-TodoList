9d9afb18cc5458db2ccb12236778ba809e549607
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2024-08-10 23:17:25 +0700

    feat(logic): add code for function addTodoList

--- BusinessLogic/AddTodoList.php
+++ BusinessLogic/AddTodoList.php
@@ -3,7 +3,13 @@
 /**
  * Menambahkan todo list
  */
-function addTodoList()
+function addTodoList(string $todo): void
 {
-  //
+  // Mengambil data dari global scope
+  global $todoList;
+
+  // Menambah index number berdasarkan jumlah data todoList
+  $number = sizeof($todoList) + 1;
+
+  $todoList[$number] = $todo;
 }
