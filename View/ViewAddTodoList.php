d811c53460e9bb8adfab378897a83d9884838623
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2024-08-13 22:43:06 +0700

    feat: add ViewAddTodoList logic and create the test

--- View/ViewAddTodoList.php
+++ View/ViewAddTodoList.php
@@ -1,8 +1,20 @@
 <?php
 
+require_once "./Model/TodoList.php";
+require_once "./BusinessLogic/AddTodoList.php";
+require_once "./Helper/Input.php";
+
 /**
  * Menampilkan view add todo list
  */
 function viewAddTodoList() {
-  //
+  echo "Menambahkan Todo" . PHP_EOL;
+
+  $todo = input("Masukkan todo (x untuk batal): ");
+  
+  if ($todo == "x") {
+    echo "Batal menambahkan todo" . PHP_EOL;
+  } else {
+    addTodoList($todo);
+  }
 }
